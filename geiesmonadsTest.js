13bce653b8090aaa1c08d29da0e8084bd01e5fa7
Marco Faustinelli <contatti@faustinelli.net>
2014-02-16 18:07:41 +0100

    TRULY CHAINED STATE MONAD!!!
    A state monad with input & output and chained clean flatMaps!!

--- geiesmonadsTest.js
+++ geiesmonadsTest.js
@@ -56,7 +56,7 @@ YAHOO.GEIESMONADS.test.oTestSMAIOM = new YAHOO.tool.TestCase({
 			);
 
 		// uncomment to run
-		//askThenInputThenGreet(0);
+		// askThenInputThenGreet(0);
 		
 		var promptThenGreet = nullState().bind(
 			function(x){ return getString('what is your name?')}).bind(
@@ -92,7 +92,7 @@ YAHOO.GEIESMONADS.test.oTestSMAIOM = new YAHOO.tool.TestCase({
 		.onError(kickAway);
 		
 		// uncomment to run
-		promptThenCheckThenGreetOrKick(0);
+		//promptThenCheckThenGreetOrKick(0);
 	}
 });
 
@@ -170,6 +170,64 @@ YAHOO.GEIESMONADS.test.oTestSMAO = new YAHOO.tool.TestCase({
 	}
 });
 
+YAHOO.GEIESMONADS.test.oTestMultipleFlatmap = new YAHOO.tool.TestCase({
+	name : "TestMultipleFlatmap",
+	testMultipleFlatmap : function() {
+	
+		var getString = Monad.state.lift(prompt);
+		var putString = Monad.state.lift(alert);
+		var nullState = function(){
+			return Monad.state.unit(function(x){return x});
+		};
+		
+/*		
+		for {
+			a <- nullState()
+			x <- getString('what is your first name?')
+			y <- getString('what is your second name?')
+			z <- putString(('welcome, ' + x + ' ' + y))
+		} yield z
+		
+		nullState
+			.flatMap(a -> getString('what is your first name?')
+				.flatMap(x -> getString('what is your second name?')
+					.flatMap(y -> putString('welcome, ' + x + ' ' + y)
+						.map(IDENTITY))))
+*/
+		
+		var askThenInputThenAskThenInputThenGreet = nullState()
+			.bind(function(a){ return getString('what is your first name?')
+				.bind(function(x){ return getString('what is your second name?')
+					.bind(function(y){ return putString('welcome, ' + x + ' ' + y)
+						//.map(function(xxx){return xxx})
+						})})});
+		
+		//askThenInputThenAskThenInputThenGreet(0);
+	
+		var getStringInState = function(msg) {
+			return function(state) {
+				var keyValue = prompt(msg).split('_');
+				state[keyValue[0]] = keyValue[1];
+				return {value: keyValue[1],state: state}
+			}
+		}
+		
+		var putStringFromState = function(msg) {
+			return function(state) {
+				var undy = alert(msg + ' ' + state.x + ' ' + state.y);
+				return {value: undy,state: state}
+			}
+		}
+		
+		var cccchain = nullState()
+			.bind(function(x){ return getStringInState('what is your first name? call it x'); })
+			.bind(function(x){ return getStringInState('what is your second name? call it y'); })
+			.bind(function(x){ return putStringFromState('welcome, '); });
+			
+		cccchain({});
+	}
+});
+
 YAHOO.util.Event
 		.onDOMReady(function() {
 
@@ -184,6 +242,9 @@ YAHOO.util.Event
 
 			YAHOO.GEIESMONADS.test.GEIESMONADS_TestSuite
 				.add(YAHOO.GEIESMONADS.test.oTestSMAO);
+
+			YAHOO.GEIESMONADS.test.GEIESMONADS_TestSuite
+				.add(YAHOO.GEIESMONADS.test.oTestMultipleFlatmap);
 
 			var logger = new YAHOO.tool.TestLogger("testLogger_GEIESMONADS");
 			logger.hideCategory("info");
