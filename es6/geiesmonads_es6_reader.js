b505505061ebeba50b651edc03d4c788982f2935
Muzietto <marco_faustinelli@yahoo.com>
2017-04-23 05:04:04 +0200

    implemented also local

--- es6/geiesmonads_es6_reader.js
+++ es6/geiesmonads_es6_reader.js
@@ -1,27 +1,31 @@
 var MONAD = {};
 
-MONAD.reader = (() => {
-  var monad = fctxa => {
-    var rea = ctx => fctxa(ctx); // rea :: ctx -> a
-    rea.bind = farmb => monad(ctx => { // rmb :: ctx -> b
-      var a = rea(ctx);
-      return farmb(a)(ctx); // rmb(ctx) :: b
+// NB: rm means 'reader monad'; type of context is ctx, not r!
+MONAD.reader = (() => { // rm a = rm ctx a = rm (ctx -> a)
+  var monad = fctxa => { // fctxa :: ctx -> a
+    var thisreader = ctx => fctxa(ctx); // thisreader :: ctx -> a
+    thisreader.bind = farmb => monad(ctx => { // rm b :: ctx -> b
+      var a = thisreader(ctx);
+      return farmb(a)(ctx); // rm b ctx :: b
     });
-    return rea;
+    return thisreader;
   }
 
-	return {
-    UNIT: x => monad(_ => x),
-    reader: monad,
-    ask: () => monad(x => x),
-    asks: fctxa => monad(fctxa)
+	return { // NB writing 'rm a' or 'rm ctx a' is the same
+    UNIT: a => monad(_ => a), // UNIT :: a -> rm a = a -> rm (ctx -> a)
+    reader: monad, // monad :: (ctx -> a) -> rm a
+    ask: _ => monad(ctx => ctx), // ask :: ctx -> rm ctx
+    asks: fctxb => monad(fctxb), // asks :: (ctx -> b) -> rm b
+    // local :: (ctx -> ctx) -> rm a -> rm a
+    local: fctxctx => rma => monad(ctx => rma(fctxctx(ctx)))
 	};
 })();
 
 var reader = MONAD.reader,
 unit = MONAD.reader.UNIT,
 ask = MONAD.reader.ask,
-asks = MONAD.reader.asks
+asks = MONAD.reader.asks,
+local = MONAD.reader.local
 ;
 
 var greet = name => ask().bind(ctx => unit(ctx + ', ' + name));
@@ -43,3 +47,10 @@ var example1 = () => {
 };
 
 example1();
+
+var example3 = () => {
+  var insult = ctx => ctx + ' old bastard';
+  console.log((local(insult)(greet('Jimmy').bind(end)))('Hello'));
+}
+
+example3();
