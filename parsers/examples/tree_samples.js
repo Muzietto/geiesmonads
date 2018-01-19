97bec5b89462d199524c1310b6398a1a19bfdf2f
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2018-01-19 15:04:01 +0100

    starting tree parsing examples, but maybe they're too complex...

---
+++ parsers/examples/tree_samples.js
@@ -0,0 +1,12 @@
+
+let tree1 = 'leaf("a")';
+
+let tree2 = 'node(leaf("a"))';
+
+let tree3 = 'node(leaf("a"),,)';
+
+let tree4 = 'node(leaf("a"),leaf("b"))';
+
+let tree5 = 'node(leaf("a"),leaf("b"),)';
+
+let tree6 = 'node(leaf("a"),leaf("b"),leaf("b"))';
