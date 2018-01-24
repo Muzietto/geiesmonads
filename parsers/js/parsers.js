5d240da256311a8abaae149b6d9d12d1584eb83b
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2018-01-24 10:46:57 +0100

    putting labels on more standard parsers

--- parsers/js/parsers.js
+++ parsers/js/parsers.js
@@ -93,11 +93,11 @@ export function anyOf(charsArray) {
         .setLabel('anyOf ' + charsArray.reduce((acc, curr) => acc + curr, ''));
 }
 
-export const lowercaseP = anyOf(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]);
-export const uppercaseP = anyOf(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]);
-export const letterP = lowercaseP.orElse(uppercaseP);
-export const digitP = anyOf(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
-export const whiteP = anyOf([' ', '\t', '\n', '\r']);
+export const lowercaseP = anyOf(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]).setLabel('lowercaseP');
+export const uppercaseP = anyOf(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]).setLabel('uppercaseP');
+export const letterP = lowercaseP.orElse(uppercaseP).setLabel('letterP');
+export const digitP = anyOf(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']).setLabel('digitP');
+export const whiteP = anyOf([' ', '\t', '\n', '\r']).setLabel('whiteP');
 
 export function fmap(fab, parser1) {
     const label = parser1.label + ' fmap ' + fab.toString();
