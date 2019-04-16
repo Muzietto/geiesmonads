1bb36089f98eb248b04f022cbe84b4eec6bfe883
Marco Faustinelli <marco_faustinelli@yahoo.com>
2019-04-16 18:31:06 +0200

    first unsatisfactorily fluent object

--- io/io.js
+++ io/io.js
@@ -1,21 +1,42 @@
 
 function compose() {
   const argz = arguments;
-  return x => [...argz].reduce((acc, curr) => curr(acc), x);
+  return x => [...argz].reduceRight((acc, curr) => curr(acc), x);
 }
 
-const addClass = name => el => el.classList.add(name);
+const addClass = name => el => {
+  el.classList.add(name);
+}
 
-const S = f => g => x => f(x)(g(x))
+const S = f => g => x => {
+  const fx = f(x);
+  const gx = g(x);
+  const result = fx(gx);
+  return result;
+}
 
-const K = x => y => x;
+const K = x => y => {
+  return x;
+}
 
 const addClassOK = name => S(K)(addClass(name));
+               //     x => K(x)(addClass(name)(x));
 
-                           x => K(x)(addClass(name)(x));
+const getElement = document.getElementById.bind(document);
 
-const getElement = document.getElementById;
+const mess = compose (addClassOK('red'), addClassOK('large'), getElement);
 
-//const mess = compose (addClassOK('red'), addClassOK('blue'), getElement);
+const mess2 = el => addClassOK('red')(addClassOK('large')(getElement(el)));
 
-const mess = compose (x => x + 'ciao', x => x + 'mamma', x => x + 'mia' );
+const newMess = el => PIPPO().GetElement(el).AddClassOK('large').AddClassOK('red');
+
+
+function PIPPO() {
+  let _target;
+  const result = {
+    target: () => _target,
+    GetElement: id => { _target = getElement(id); return result; },
+    AddClassOK: name => { addClassOK(name)(result.target()); return result; },
+  };
+  return result;
+}
