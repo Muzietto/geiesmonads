0fc84f6360e49ad7ba927d33c69c7bdebd351579
Marco Faustinelli <marco_faustinelli@yahoo.com>
2019-04-18 12:26:43 +0200

    some acrobatics with compose and map

--- io/flickr.js
+++ io/flickr.js
@@ -47,17 +47,41 @@ require([
         t + '&format=json&jsoncallback=?';
     };
 
-    var mediaUrl_orig = compose(prop('m'), prop('media'));
+
+    const ddata = {
+      "items": [
+  	   {
+  			"media": {"m":"https:\/\/live.staticflickr.com\/65535\/40654970983_a19a4b6699_m.jpg"},
+  	   },
+  	   {
+  			"media": {"m":"https:\/\/live.staticflickr.com\/7816\/32671123767_317df1bea4_m.jpg"},
+  	   }
+     ],
+    };
+
+    var mediaUrl_orig = compose(prop('m'), prop('media'));  // mappatore per items
 
     const mediaUrl = obj => prop('m')(prop('media')(obj));
 
     var srcs_orig = compose(map(mediaUrl), prop('items'));
 
-    const srcs = x => map(obj => prop('m')(prop('media')(obj)))(prop('items')(x))
+    const srcs = x => map(obj => prop('m')(prop('media')(obj)))(prop('items')(x));
 
     var images_orig = compose(map(img), srcs);
 
-    const images = ys => map(img)((x => map(obj => prop('m')(prop('media')(obj)))(prop('items')(x)))(ys));
+    var imagesa = compose(map(img), compose(map(mediaUrl), prop('items')));
+
+    var imagesb = composeN(map(img), map(mediaUrl), prop('items'));
+
+    var imagesc = compose(map(compose(img, mediaUrl)), prop('items'));
+
+    const objToImage = compose(img, mediaUrl);
+
+    var images = compose(map(objToImage), prop('items'));
+
+    const imageszzz = ys => map(img)((x => map(obj => prop('m')(prop('media')(obj)))(prop('items')(x)))(ys));
+
+    // var law = compose(map(f), map(g)) === map(compose(f, g));
 
     var renderImages_orig = compose(Impure.setHtml('body'), images);
 
