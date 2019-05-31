dcb9d87a38bcece8cc3d6241a39bc4753b3170ec
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-05-31 17:35:06 +0200

    main routine for CSS git log file parsing

---
+++ parsers-node/css-metrics/parseCssLogfile.js
@@ -0,0 +1,20 @@
+
+// NB this is a copy of another main file for another task
+// adaptation to git log parsing must still be done
+
+import fs from 'fs';
+import readline from 'readline';
+import stream from 'stream';
+
+const instream = fs.createReadStream('./icon_name_occurrences.txt');
+
+const rl = readline.createInterface({ input: instream });
+
+const THE_REGEX = /^.*icon=\'(.+?)'.*$/gm;
+
+rl.on('line', line => {
+  const match = THE_REGEX.exec(line);
+  if (match !== null) console.log(match[1]);
+});
+
+// $ npm run start | sort | uniq
