0b730a01276efeb6b327577f540d619e09083d67
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-06-03 14:36:13 +0200

    first very drafty tests

---
+++ parsers-node/css-metrics/git_log_parser_tests.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import {
+  lineP
+} from './git_log_parser';
+
+describe('among git log parsers', () => {
+
+    describe('lineP', () => {
+        it('runs a parser and then expects a CR immediately after', () => {
+            const line = lineP(pchar('a'));
+            expect(line.run('a').isSuccess).to.be.false;
+            expect(line.run('a\n').isSuccess).to.be.true;
+        });
+    });
+});
