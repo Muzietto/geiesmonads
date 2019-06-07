cd742212c5ae8a3cbbacdf89fcf3ba44152abfd1
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-06-07 11:37:56 +0200

    specs for git log parsers up to whole lines

--- parsers-node/css-metrics/src/test/git_log_parser.spec.js
+++ parsers-node/css-metrics/src/test/git_log_parser.spec.js
@@ -2,26 +2,88 @@ import { expect } from 'chai';
 import {
   lineP,
   numberP,
+  whateverP,
+  weekdayP,
+  yearP,
+  timezoneP,
+  daytimeP,
+  dateP,
 } from '../git_log_parser';
 import {
   pchar,
-  logP,
-  digitP,
+  logP
 } from '../lib/parsers';
 
 describe('among git log parsers', () => {
 
-    describe.only('lineP', () => {
+    describe('lineP', () => {
         it('runs a parser and then expects a CR immediately after', () => {
             const line = lineP(pchar('a'));
-            console.log(line.run('a\n').toString());
-            expect(line.run('a\n').isSuccess).to.be.true;
+            expect(line.run('a').isSuccess).to.be.false;
+            expect(line.run(prep('a\n')).isSuccess).to.be.true;
         });
     });
 
     describe('numberP', () => {
-      it('runs a parser and then expects a CR immediately after', () => {
-          console.log(numberP.run('123').toString());
-      });
-  });
+        it('parses chars and returns integers', () => {
+            expect(numberP.run('').isSuccess).to.be.false;
+            expect(numberP.run('a').isSuccess).to.be.false;
+            expect(numberP.run('123').isSuccess).to.be.true;
+        });
+    });
+
+    describe('whateverP', () => {
+        it('parses anything, as long as it\'s made of chars', () => {
+            expect(whateverP.run('').isSuccess).to.be.true;
+            expect(whateverP.run('a').isSuccess).to.be.true;
+            expect(whateverP.run('123').isSuccess).to.be.true;
+            expect(whateverP.run('123asd£$% £$%345ertERT QWEQWE123@#ù').isSuccess).to.be.true;
+        });
+    });
+
+    describe('weekdayP', () => {
+        it('parses the shortened days of the week in English', () => {
+            expect(weekdayP.run('Lun').isSuccess).to.be.false;
+            expect(weekdayP.run('Mon').isSuccess).to.be.true;
+            expect(weekdayP.run('Sun').isSuccess).to.be.true;
+        });
+    });
+
+    describe('yearP', () => {
+        it('parses 4 digits and returns a string', () => {
+            expect(yearP.run('123').isSuccess).to.be.false;
+            expect(yearP.run('123e').isSuccess).to.be.false;
+            expect(yearP.run('1234').isSuccess).to.be.true;
+        });
+    });
+
+    describe('timezoneP', () => {
+        it('wants a plus before a year', () => {
+            expect(timezoneP.run('+123').isSuccess).to.be.false;
+            expect(timezoneP.run('+123e').isSuccess).to.be.false;
+            expect(timezoneP.run('+1234').isSuccess).to.be.true; // arr.join is not a function
+            expect(timezoneP.run('-1234').isSuccess).to.be.false;
+        });
+    });
+
+    describe('daytimeP', () => {
+        it('parses digits and colons', () => {
+            expect(daytimeP.run('01:02:03').isSuccess).to.be.true;
+            expect(daytimeP.run('1234').isSuccess).to.be.false;
+            expect(daytimeP.run('01.02.03').isSuccess).to.be.false;
+        });
+    });
+
+    describe('dateP', () => {
+        it('parses a complex string and returns a JS Date', () => {
+          expect(dateP.run('Tue Jan 22 15:38:24 2019 +0100').isSuccess).to.be.true;
+          expect(dateP.run('Tue Jan 22 15:38:24 2019').isSuccess).to.be.false;
+          expect(dateP.run('Tue Jan 22 15:38:24 2019 +0100').value[0].constructor.name === 'Date').to.be.true;
+        });
+    });
+
 });
+
+function prep(str) {
+  return str.replace('\n', '§§')
+}
