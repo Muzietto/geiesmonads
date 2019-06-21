2627d1f2e602619f19e9b4294dd94017f0e991d8
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-06-21 16:53:46 +0200

    completed script for CSS git logfiles

--- parsers-node/css-metrics/parseGitLogfile.js
+++ parsers-node/css-metrics/parseGitLogfile.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import readline from 'readline';
 import stream from 'stream';
-import { gitLogFileP } from './src/git_log_parser';
+import { prettyLogP } from './src/git_log_parser';
 
 console.log('starting...');
-const instream = fs.createReadStream('./pippo2.txt');
+const instream = fs.createReadStream('./css_input/input02.txt');
 
 const rl = readline.createInterface({ input: instream });
+const ws = fs.createWriteStream('./css_output/output02.csv');
 
 let buffer = '';
 
@@ -19,8 +20,13 @@ rl.on('line', line => {
 rl.on('close', line => {
   console.log('closing...');
   console.log('input is ', buffer);
-  const result = gitLogFileP.run(buffer);
-  console.log('----->', result.toString());
+  const result = prettyLogP.run(buffer);
+  ws.write(pretty(result.value[0]));
+  console.log('---RES-->', pretty(result.value[0]));
 });
 
 // $ npm run start
+
+function pretty(input) {
+  return input.toString();
+}
