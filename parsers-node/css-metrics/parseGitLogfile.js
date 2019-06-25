3cee458ddf8c1c43f0cad4d52c2af3db4fd4a9de
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-06-25 10:02:38 +0200

    after parsing JS logfiles

--- parsers-node/css-metrics/parseGitLogfile.js
+++ parsers-node/css-metrics/parseGitLogfile.js
@@ -4,22 +4,22 @@ import stream from 'stream';
 import { prettyLogP } from './src/git_log_parser';
 
 console.log('starting...');
-const instream = fs.createReadStream('./css_input/input02.txt');
+const instream = fs.createReadStream('./js_input/pippo4.txt');
 
 const rl = readline.createInterface({ input: instream });
-const ws = fs.createWriteStream('./css_output/output02.csv');
+const ws = fs.createWriteStream('./js_output/pippo4.csv');
 
 let buffer = '';
 
 rl.on('line', line => {
-  console.log('line is ', line);
+  //console.log('line is ', line);
   buffer += (line + '§§');
 });
 
 
 rl.on('close', line => {
   console.log('closing...');
-  console.log('input is ', buffer);
+  //console.log('input is ', buffer);
   const result = prettyLogP.run(buffer);
   ws.write(pretty(result.value[0]));
   console.log('---RES-->', pretty(result.value[0]));
