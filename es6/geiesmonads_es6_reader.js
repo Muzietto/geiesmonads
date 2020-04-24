484ce20d109bc519b53f785f494dd913c08c7b8c
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2020-04-24 07:11:27 +0200

    READER MONAD: first serious own implementation, starting MEANINGFUL tests

--- es6/geiesmonads_es6_reader.js
+++ es6/geiesmonads_es6_reader.js
@@ -1,18 +1,40 @@
+/*
+	GEIESMONADS - JS monads
+	Author: Marco Faustinelli (contacts@faustinelli.net)
+	Web: http://faustinelli.net/
+	     http://faustinelli.wordpress.com/
+
+	The MIT License - Copyright (c) 2013/2020 Geiesmonads Project
+*/
+
 var MONAD = {};
 
 // NB: rm means 'reader monad'; type of context is ctx, not r!
 MONAD.reader = (() => { // rm a = rm ctx a = rm (ctx -> a)
-  var monad = fctxa => { // fctxa :: ctx -> a
-    var thisreader = ctx => fctxa(ctx); // thisreader :: ctx -> a
-    thisreader.bind = farmb => monad(ctx => { // rm b :: ctx -> b
-      var a = thisreader(ctx);
-      return farmb(a)(ctx); // rm b ctx :: b
+  var monad = fra => { // fra :: ctx -> a
+    var thisreader = r => fra(r); // gotta CLONE fra in order to decorate it later!!
+    // fmap :: (a -> b) -> (r -> a) -> (r -> b)
+    thisreader.fmap = fab => monad(r => { // fab :: a -> b
+      return fab(fra(r));
+    });
+    // ap :: m(a -> b) -> ma -> mb
+    thisreader.ap = Rra => monad(r => { // will return a Rrb
+      const fr_ab = fra;  // NB in this case fra :: r -> (a -> b)
+      const a = Rra(r);
+      const fab = fr_ab(r);
+      const b = fab(a);
+      return fr_ab(r)(Rra(r)) // Rr_ab -> Rra -> Rrb
+    });
+    thisreader.bind = farmb => monad(r => { // rm b :: ctx -> b
+      var a = fra(r);
+      return farmb(a)(r); // rm b r :: b
     });
     return thisreader;
   };
 
   return { // NB writing 'rm a' or 'rm ctx a' is the same
-    UNIT: a => monad(_ => a), // UNIT :: a -> rm a = a -> rm (ctx -> a)
+    UNIT: a => monad(() => a), // UNIT :: a -> rm a = a -> rm (ctx -> a)
+    pure: a => monad(() => a),
     reader: monad, // monad :: (ctx -> a) -> rm a
     ask: _ => monad(ctx => ctx), // ask :: ctx -> rm ctx
     asks: fctxb => monad(fctxb), // asks :: (ctx -> b) -> rm b
--- es6/geiesmonads_es6_reader_test.js
+++ es6/geiesmonads_es6_reader_test.js
@@ -17,65 +17,24 @@ describe('the reader monad', function() {
   asks = MONAD.reader.asks,
   local = MONAD.reader.local;
 
-  var greet = name => ask().bind(ctx => {
-    //debugger;
-    return unit(ctx + ', ' + name);
-  });
-
   it('can receive and use a context provided at runtime', function() {
-    expect(greet('Marco')('Ciao')).to.be.equal('Ciao, Marco');
-    expect(greet('Marco')('Eccoti qua')).to.be.equal('Eccoti qua, Marco');
+    expect(ask()
+      .bind(x => unit(x + ', '))
+      .bind(x => unit(x + 'Marco'))('Ciao')).to.be.equal('Ciao, Marco');
   });
 
-  var completeTheGreeting = str => {
-    //debugger;
-    var isCtxHello = ctx => (ctx === 'Hello');
-    return asks(isCtxHello).bind(isHello => {
-        //debugger;
-        return unit(str + (isHello ? '!!' : '...'));
-      });
-  };
+  it('can operate on a locally modified context', function() {
+    expect(local(s => s + 'Sauce')(ask())('Chocolate'))
+      .to.be.equal('ChocolateSauce');
 
-  it('can act upon the context value', function() {
-    // ctx(greet||completeTheGreeting) = 'Hello'
-    expect(greet('John').bind(completeTheGreeting)('Hello')).to.be.equal('Hello, John!!');
-    var greetCompletely = name => greet(name).bind(completeTheGreeting);
-    // ctx(greet||completeTheGreeting) = 'Hi'
-    expect(greetCompletely('Trudy')('Hi')).to.be.equal('Hi, Trudy...');
+    expect(local(x => 'sauce' + x)
+      (ask().bind(s => unit('ciao' + s)))('birillo'))
+        .to.be.equal('ciaosaucebirillo');
+
   });
 
-  var insult = ctx => {
-    //debugger; // darn interesting observation point
-    return ctx + ' old bastard';
-  }
 
-  it('can operate in a locally modified context', function() {
-    // ctx(insult) = 'Hello', ctx(greet||completeTheGreeting) = 'Hello old bastard'
-    expect((local(insult)(greet('John').bind(completeTheGreeting)))('Hello'))
-      .to.be.equal('Hello old bastard, John...');
-    // ctx(insult||completeTheGreeting) = 'Hello', ctx(greet) = 'Hello old bastard'
-    expect((local(insult)(greet('John'))).bind(completeTheGreeting)('Hello'))
-      .to.be.equal('Hello old bastard, John!!');
-    // ctx(greet||completeTheGreeting) = 'Hello'
-    // === WTF?! ===> ctx(insult) = 'Hello, John' <=== WTF?! ===
-    expect(greet('John').bind(local(insult)(completeTheGreeting))('Hello'))
-      .to.be.equal('Hello, John old bastard!!');
-  });
 
-  it('has some surprising quirks which can catch people unaware, or at least me...', function() {
-    // ctx(greet1||completeTheGreeting1||completeTheGreeting2) = 'Hello'
-    // str(completeTheGreeting1) = 'Hello, John'
-    // str(completeTheGreeting2) = 'Hello, John!!'
-    expect(greet('John').bind(completeTheGreeting).bind(completeTheGreeting)('Hello'))
-      .to.be.equal('Hello, John!!!!');
-    // ctx(greet1||completeTheGreeting) = 'Hello'
-    // str(completeTheGreeting) = 'Hello, John'
-    // === AAA ===> name(greet2) = 'Hello, John!!' <=== AAA ===
-    expect(greet('John').bind(completeTheGreeting).bind(greet)('Hello'))
-      .to.be.equal('Hello, Hello, John!!');
-    // str(completeTheGreeting) = 'Louie'
-    // ctx(completeTheGreeting||greet) = 'Hello'
-    // === AAA ===> name(greet) = 'Louie!!' <=== AAA ===
-    expect(completeTheGreeting('Louie').bind(greet)('Hello')).to.be.equal('Hello, Louie!!');
-  });
+
+
 });
